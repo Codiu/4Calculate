991ca22d03ba25fc65093e0e58ebe3a856e9a11d
Evgeniy Korolkov <26188987+Codiu@users.noreply.github.com>
2023-01-17 22:39:37 +0500

    Create calculator from scratch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
 
-
-
-
-
-
-
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Input 2 item expression: ");
+        String input = myObj.nextLine();
+
+        // clean spaces
+        input = input.replaceAll("\\s+","");
+        int result;
+        double resultDouble;
+        // traverse expression
+        for (int i = 1; i < input.length(); i++) {
+            switch (input.charAt(i)) {
+                case '+':
+                    result = Integer.parseInt(input.substring(0, i)) + Integer.parseInt(input.substring(i + 1));
+                    System.out.println(result);
+                    break;
+                case '-':
+                    result = Integer.parseInt(input.substring(0, i)) - Integer.parseInt(input.substring(i + 1));
+                    System.out.println(result);
+                    break;
+                case '/':
+                    resultDouble = Double.parseDouble(input.substring(0, i)) / Integer.parseInt(input.substring(i + 1));
+                    System.out.println(resultDouble);
+                    break;
+                case '*':
+                    result = Integer.parseInt(input.substring(0, i)) * Integer.parseInt(input.substring(i + 1));
+                    System.out.println(Integer.parseInt(input.substring(0, i)) + " * " + Integer.parseInt(input.substring(i + 1)) + " = " + result);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
